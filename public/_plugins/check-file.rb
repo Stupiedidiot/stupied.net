2636b1f367a8ad4e50f7f5225e408c940bea4b77
Stupiedidiot <115616307+Stupiedidiot@users.noreply.github.com>
2026-05-20 19:26:10 +0800

    updated check file script

--- public/_plugins/check-file.rb
+++ public/_plugins/check-file.rb
@@ -1,8 +1,10 @@
 module Jekyll
   module FileExistsFilter
-    def check_file(input)
+    def check_file(input, default = nil)
       if (File.exist?(File.join(@context.registers[:site].source, input)) )
         return input
+      elsif (default)
+        return default
       else
         return nil
       end
