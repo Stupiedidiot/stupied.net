5547ee988facf58e3526cc38dc10a69f221d291a
Stupiedidiot <115616307+Stupiedidiot@users.noreply.github.com>
2026-06-23 20:25:28 +0800

    moved templates out

--- public/snail/_templates/literalhat/main.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    let e = document.querySelector('#container > nav');
-    if (!e.classList.contains('open')) {
-        e.classList.add('open')
-    } else {
-        e.classList.remove('open')
-    }
-}
