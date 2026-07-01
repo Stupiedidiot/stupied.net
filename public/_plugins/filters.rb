0aaf4f6ee6422211fef36f0e0277d93e794be1bc
Stupiedidiot <115616307+Stupiedidiot@users.noreply.github.com>
2026-07-01 12:09:48 +0800

    finally getting somewhere, fuck yeah

--- public/_plugins/filters.rb
+++ public/_plugins/filters.rb
@@ -11,32 +11,11 @@ module Jekyll
         return "TITLE UNKNOWN"
       end
     end
-  
-    def filterFuture(input)
-      return [] unless input.is_a?(Array)
-      date =  @context.registers[:site].time.to_i
 
-      # UNCOMMENT FOR DEBUGGING!
-      #puts "#{@context.registers[:page]['path']} ==================="
-      
-      input.each.with_index(0) do |e, i|
-        #puts "#{i}: #{e['title']||e['img']||""}"
-        
-        date_current = e['date']
-        if date_current.is_a?(String)
-          date_current = Time.parse(date_current).to_i 
-        end
-
-        if (date_current > date)
-          input.delete_at(i)
-        else
-          break
-        end
-
-      end
-
-      return input
+    def sort_tags(tags)
+      tags.sort_by { |tag, posts| [-posts.size, tag.downcase] }
     end
+  
 
   end
 end
